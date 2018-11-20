f89485158b273392d849298aa72660ab6f2cc353
alberto bortoni <alb.deg@gmail.com>
2018-11-20 18:39:35 -0500

    annother commit

--- app/src/main/java/bortoni/mx/read_faster/bookFiles.java
+++ app/src/main/java/bortoni/mx/read_faster/bookFiles.java
@@ -8,14 +8,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
+
 public class bookFiles extends AppCompatActivity {
+
+    //public static File bookFiles = Environment.getExternalStoragePublicDirectory(En)
+
     private static final String TAG = "bookFiles";
 
     //--------init class variables--------//
     private ArrayList<String> mTitles = new ArrayList<>();
     private ArrayList<String> mImages = new ArrayList<>();
+    //File bookDir = getDir("bookFileDir", MODE_PRIVATE);
+
+    //Field[] bookFiles = R.raw.class.getFields();
+    //File rawDir = new File(R.raw.class);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +34,20 @@ public class bookFiles extends AppCompatActivity {
         setContentView(R.layout.book_files);
         Log.d(TAG, "onCreate: Started bookfiles");
 
+
+
+
+
+        //--------get files
+        fetchFiles();
+        initRecyclerView();
+
         //--------buttons
         Button addFile = (Button) findViewById(R.id.b_add);
         addFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                addBookFile();
             }
         });
 
@@ -37,7 +55,7 @@ public class bookFiles extends AppCompatActivity {
         deleteFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                deleteBookFile();
             }
         });
 
@@ -45,22 +63,53 @@ public class bookFiles extends AppCompatActivity {
         selectFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                selectTargetFile();
             }
         });
 
 
 
-    //--------get files
-        fetchFiles();
-        initRecyclerView();
+
 
     }
 
+///////////////////////////////////////////////////////////////////
+//                      OTHER FUNCTIONS                          //
+//---------------------------------------------------------------//
 
+    //--------switch to activities--------//
     private void fetchFiles(){
         Log.d(TAG, "fetchFiles: retrieving flies");
-        mTitles.add("one");
+
+        //File bookFilesa = new File("android.resource://" + getPackageName() + "/" + R.raw.class.getName());
+        //File[] bookFiles = bookFilesa.listFiles();
+
+        Field[] bookFiles = R.raw.class.getFields();
+
+
+        mTitles.add(bookFiles[0].getName());
+        mImages.add("@android:drawable/ic_menu_sort_by_size");
+
+        mTitles.add(bookFiles[1].getName());
+        mImages.add("@android:drawable/ic_menu_sort_by_size");
+
+        mTitles.add(bookFiles[2].getName());
+        mImages.add("@android:drawable/ic_menu_sort_by_size");
+
+
+ /*       //String[] myList = bookDir.list();
+
+        mTitles.add(myList[0]);
+        mImages.add("@android:drawable/ic_menu_sort_by_size");
+
+        mTitles.add(myList[1]);
+        mImages.add("@android:drawable/ic_menu_sort_by_size");
+
+        mTitles.add(myList[2]);
+        mImages.add("@android:drawable/ic_menu_sort_by_size");
+*/
+
+  /*      mTitles.add("one");
         mImages.add("@android:drawable/ic_menu_sort_by_size");
 
         mTitles.add("two");
@@ -85,7 +134,7 @@ public class bookFiles extends AppCompatActivity {
         mImages.add("@android:drawable/ic_menu_sort_by_size");
 
         mTitles.add("nine");
-        mImages.add("@android:drawable/ic_menu_sort_by_size");
+        mImages.add("@android:drawable/ic_menu_sort_by_size");*/
 
     }
 
@@ -96,4 +145,20 @@ public class bookFiles extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    //--------button services--------//
+
+    private void addBookFile(){
+
+    }
+
+    private void deleteBookFile(){
+
+    }
+
+    private void selectTargetFile(){
+
+    }
+
+
 }
